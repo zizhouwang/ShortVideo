dde5394de743efebe73c1d987939e2716277a955
wangzizhou <wangzizhou@zxianstudio.com>
2018-09-03 21:12:05 +0800

    poptiner

---
+++ PopTiner/PopTiner/BaseObject/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  PopTiner
+//
+//  Created by zizhou wang on 2018/9/3.
+//  Copyright © 2018年 zizhou wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
